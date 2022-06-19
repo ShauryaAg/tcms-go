70579e24a7426099ec366d4a5721a0d2db5d407e
Shaurya <agarwal.lucky.shaurya11@gmail.com>
2022-06-19 23:55:06 +0530

    feat (http): add `TestCaseHandler`

---
+++ internal/http/handler.go
@@ -0,0 +1,50 @@
+package http
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/ShauryaAg/tcms-go/pkg/repository/interfaces"
+)
+
+type TestCaseHandler struct {
+	Store interfaces.Repository
+}
+
+func (h *TestCaseHandler) CreateTestCase(w http.ResponseWriter, r *http.Request) {
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var data map[string]interface{}
+	err = json.Unmarshal(bodyBytes, &data)
+	if err != nil {
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	testcase, err := h.Store.Create(r.Context(), data)
+	if err != nil {
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	jsonBytes, err := json.Marshal(testcase)
+	if err != nil {
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
