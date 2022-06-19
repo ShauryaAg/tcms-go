88ede7da0a6b3cd97d2e0400c8f029d3bd1dc836
Shaurya <agarwal.lucky.shaurya11@gmail.com>
2022-06-20 02:14:20 +0530

    feat: add `database` and `client` in `MongoStore`

--- internal/db/mongo_store.go
+++ internal/db/mongo_store.go
@@ -15,23 +15,33 @@ var (
 	connectionUri = uri
 )
 
-type MongoStore struct {
-	DB *mongo.Database
+func NewMongoStore(database string) *MongoStore {
+	return &MongoStore{
+		DatabaseName: database,
+	}
 }
 
-func (m *MongoStore) Connect(ctx context.Context) (*mongo.Client, error) {
+type MongoStore struct {
+	DatabaseName string
+	database     *mongo.Database
+	client       *mongo.Client
+}
+
+func (m *MongoStore) Connect(ctx context.Context) (interface{}, error) {
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connectionUri))
 	if err != nil {
 		return nil, err
 	}
 
+	m.database = client.Database(m.DatabaseName)
+	m.client = client
 	return client, nil
 }
 
 func (m *MongoStore) Disconnect(ctx context.Context) error {
-	return nil
+	return m.client.Disconnect(ctx)
 }
 
 func (m *MongoStore) Database(ctx context.Context) interface{} {
-	return m.DB
+	return m.database
 }
