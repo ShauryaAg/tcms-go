95bf5f9b4bb632ff627cd86b7488dc704f102663
Shaurya <agarwal.lucky.shaurya11@gmail.com>
2022-06-20 23:03:16 +0530

    feat (handler): add `GetTestCase` handler

--- internal/http/test_case.go
+++ internal/http/test_case.go
@@ -6,11 +6,13 @@ import (
 	"net/http"
 
 	utils "github.com/ShauryaAg/tcms-go/pkg/http"
-	"github.com/ShauryaAg/tcms-go/pkg/repository/interfaces"
+	httpinterfaces "github.com/ShauryaAg/tcms-go/pkg/http/interfaces"
+	repointerfaces "github.com/ShauryaAg/tcms-go/pkg/repository/interfaces"
 )
 
 type TestCaseHandler struct {
-	Repository interfaces.Repository
+	Router     httpinterfaces.Router
+	Repository repointerfaces.Repository
 }
 
 func (h *TestCaseHandler) CreateTestCase(w http.ResponseWriter, r *http.Request) {
@@ -35,6 +37,25 @@ func (h *TestCaseHandler) CreateTestCase(w http.ResponseWriter, r *http.Request)
 	}
 
 	jsonBytes, err := json.Marshal(testcase)
+	if err != nil {
+		utils.Error(w, r, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func (h *TestCaseHandler) GetTestCase(w http.ResponseWriter, r *http.Request) {
+	objectId := h.Router.URLParam(*r, "ID")
+	model, err := h.Repository.FindById(r.Context(), objectId)
+	if err != nil {
+		utils.Error(w, r, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	jsonBytes, err := json.Marshal(model)
 	if err != nil {
 		utils.Error(w, r, err.Error(), http.StatusInternalServerError)
 		return
